fe2fd1d0dde259e75cd784dd05826b63e48c6fdf
iZcy <yitzhakedmundtiomanalu@mail.ugm.ac.id>
2023-09-16 19:23:10 +0700

    <fix(aboutus): desktop responsive vismis>

--- src/components/aboutus/VisMis.js
+++ src/components/aboutus/VisMis.js
@@ -35,13 +35,13 @@ export default function VisMis() {
         {/* Title */}
         <div className="flex items-end">
           <FaBriefcase className="text-primary" size="50" />
-          <p className="font-latoBoldItalic text-lightWhite text-[28px]/[24px] ml-[20px]">
+          <p className="font-latoBoldItalic text-lightWhite text-[2vw]/[1.9vw] ml-[20px] 2xl:text-[31px]/[29px]">
             {title}
           </p>
         </div>
         {/* Content */}
         <div className="mt-[20px]">
-          <p className="font-latoRegular text-lightWhite text-[20px]/[28px]">
+          <p className="font-latoRegular text-lightWhite text-[1.3vw]/[1.7vw] 2xl:text-[20px]/[26px]">
             {content}
           </p>
         </div>
@@ -56,33 +56,35 @@ export default function VisMis() {
       {createBackground("dark")}
 
       {/* Content */}
-      <div className="w-full h-screen flex flex-col justify-center items-center px-[90px]">
-        {/* Title */}
-        <div className="w-full">
-          <h1 className="text-center text-primary text-[64px]/[60px] font-avenirBlack">
-            {"180DC UGM"}
-            <br />
-            {"Vision & Mission"}
-          </h1>
-        </div>
-        {/* Vision */}
-        <div className="flex mt-[40px] w-full">
-          <h2 className="font-avenirBlack text-secondary text-[36px]/[30px] w-[14.2%]">
-            {"Vision"}
-          </h2>
-          <p className="font-latoRegular text-lightWhite text-justify text-[24px]/[32px] w-[85.8%]">
-            {vision}
-          </p>
-        </div>
-        {/* Mision */}
-        <div className="flex mt-[28px] w-full">
-          <h2 className="font-avenirBlack text-secondary text-[36px]/[30px] w-[14.2%] mt-[10px]">
-            {"Mision"}
-          </h2>
-          <div className="flex w-[85.8%] gap-[120px]">
-            {titles.map((title, idx) => {
-              return missionFormat(title, values[idx]);
-            })}
+      <div className="hidden lg:flex w-full h-screen items-center justify-center">
+        <div className="flex flex-col justify-center items-center px-[6vw] 2xl:w-[1300px] 2xl:px-0">
+          {/* Title */}
+          <div className="w-full">
+            <h1 className="text-center text-primary text-[4vw]/[3.8vw] font-avenirBlack 2xl:text-[61px]/[58px]">
+              {"180DC UGM"}
+              <br />
+              {"Vision & Mission"}
+            </h1>
+          </div>
+          {/* Vision */}
+          <div className="flex mt-[40px] w-full">
+            <h2 className="font-avenirBlack text-secondary text-[2.3vw]/[1.9vw] w-[14.2%] 2xl:text-[35px]/[29px]">
+              {"Vision"}
+            </h2>
+            <p className="font-latoRegular text-lightWhite text-justify text-[1.5vw]/[2vw] w-[85.8%] 2xl:text-[23px]/[30px]">
+              {vision}
+            </p>
+          </div>
+          {/* Mision */}
+          <div className="flex mt-[28px] w-full">
+            <h2 className="font-avenirBlack text-secondary text-[2.3vw]/[1.9vw] w-[14.2%] 2xl:text-[35px]/[29px]">
+              {"Mision"}
+            </h2>
+            <div className="flex w-[85.8%] gap-[120px]">
+              {titles.map((title, idx) => {
+                return missionFormat(title, values[idx]);
+              })}
+            </div>
           </div>
         </div>
       </div>
