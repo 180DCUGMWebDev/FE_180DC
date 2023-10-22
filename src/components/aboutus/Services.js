e23a70f372123d1e476f41a0697be933e9f6eacb
iZcy <yitzhakedmundtiomanalu@mail.ugm.ac.id>
2023-10-23 04:28:02 +0700

    <feat(aboutus): proto services minus swiper>

--- src/components/aboutus/Services.js
+++ src/components/aboutus/Services.js
@@ -72,28 +72,31 @@ export default function Services() {
     <section className="relative">
       {/* Background */}
       {createBackground("dark")}
-      <div className="hidden lg:flex w-full h-screen justify-center items-center">
-        <div className="flex w-full h-full flex-col justify-center items-center gap-[60px] pt-[20px] pb-[30px] px-[50px] 2xl:w-[1536px] 2xl:h-fit">
-          <div className="flex w-full h-[28%]">
+      <div className="flex w-full h-screen justify-center items-center">
+        <div className="flex w-full h-fit lg:h-full flex-col justify-center items-center lg:gap-[60px] pt-[20px] pb-[30px] px-[6vw] lg:px-[50px] 2xl:w-[1536px] 2xl:h-fit">
+          <div className="flex flex-col lg:flex-row w-full h-fit lg:h-[28%] max-lg:gap-[1vh]">
             {/* Titles */}
-            <div className="flex w-5/12 items-end">
-              <h1 className="text-start text-primary text-[4vw]/[3.9vw] font-avenirBlack 2xl:text-[61px]/[60px]">
+            <div className="flex lg:w-5/12 items-end">
+              <h1 className="text-start text-primary text-[8vw]/[7.6vw] lg:text-[4vw]/[3.9vw] font-avenirBlack 2xl:text-[61px]/[60px]">
                 {"Our Services"}
                 <br />
                 {"Offered for You"}
               </h1>
             </div>
-            <div className="flex w-7/12 items-end">
-              <p className="font-latoRegular text-lightWhite text-[2vw]/[2.67vw] 2xl:text-[30.7px]/[41px]">
+            <div className="flex lg:w-7/12 items-end">
+              <p className="font-latoRegular text-lightWhite text-justify text-[3vw] lg:text-[2vw]/[2.67vw] 2xl:text-[30.7px]/[41px]">
                 {content.desc}
               </p>
             </div>
           </div>
-          <div className="flex grow w-full h-fit justify-center gap-[3vw] 2xl:gap-[72px]">
-            {/* Cards */}
+          <div className="flex w-full h-fit justify-center mt-[4vw] lg:gap-[3vw] 2xl:gap-[72px]">
+            {/* Cards DESKTOP */}
             {titles.map((val, idx) => {
               return (
-                <div key={idx} className="relative flex items-end h-[360px] max-h-full w-full rounded-t-[20px] bg-gradient-to-b from-transparent from-20% to-black to-[58%] p-[1.5vw] pb-0 overflow-clip 2xl:h-[440px] 2xl:p-[30px]">
+                <div
+                  key={idx}
+                  className="relative hidden lg:flex items-end h-[360px] max-h-full w-6/12 rounded-t-[20px] bg-gradient-to-b from-transparent from-20% to-black to-[58%] p-[1.5vw] pb-0 overflow-clip 2xl:h-[440px] 2xl:p-[30px]"
+                >
                   {/* Background */}
                   <div
                     className={
@@ -116,6 +119,47 @@ export default function Services() {
                 </div>
               );
             })}
+
+            {/* Cards MOBILE */}
+            {titles.map((val, idx) => {
+              return (
+                <div
+                  key={idx}
+                  className="relative lg:hidden flex items-end h-[160px] max-h-full w-full rounded-t-[20px] bg-gradient-to-b from-transparent from-20% to-black to-[58%] p-[1.5vw] pb-0 overflow-clip"
+                >
+                  {/* Background */}
+                  <div
+                    className={
+                      "absolute -z-[1] w-[150%] " + values[idx].position
+                    }
+                  >
+                    <ImgF src={values[idx].src} alt={values[idx].alt} />
+                  </div>
+                  {/* Content */}
+                  <div className="flex flex-col justify-start text-start w-full h-[52%] 2xl:h-[187px]">
+                    <h2 className="text-secondary font-avenirBlack text-[1.9vw] 2xl:text-[29px]">
+                      {val}
+                    </h2>
+                    <ul className="w-full list-disc text-lightWhite font-latoRegular leading-[1.2] text-[1.15vw] 2xl:text-[17.6px] pl-[1.9vw] 2xl:pl-[30px]">
+                      {values[idx].values.map((value) => {
+                        return <li key={value + "_" + idx}>{value}</li>;
+                      })}
+                    </ul>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <div className="lg:hidden flex w-full justify-center items-center">
+            <p className="text-lightWhite font-latoBoldItalic text-[3vw] grow text-start">
+              {"<<"}
+            </p>
+            <p className="text-lightWhite font-latoBoldItalic text-[3vw] grow text-center">
+              {"Swipe for More Information!"}
+            </p>
+            <p className="text-lightWhite font-latoBoldItalic text-[3vw] grow text-end">
+              {">>"}
+            </p>
           </div>
         </div>
       </div>
