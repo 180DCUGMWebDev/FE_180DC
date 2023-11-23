fa6774aa7a44f92b8db4807e3caacda8480a2a14
Anantadhirya <anantadhirya@gmail.com>
2023-11-23 17:50:54 +0700

    <feat(telescope): article pages>

---
+++ src/app/(pages)/telescope/[article]/page.js
@@ -0,0 +1,37 @@
+"use client";
+import LookForward from "@/components/misc/LookForward";
+import { notFound } from "next/navigation";
+import { articles } from "@/components/content";
+import Image from "next/image";
+
+export default function Article({ params }) {
+  const article = articles.find((article) => article.id == params.article);
+  if (!article) notFound();
+  return (
+    <main className="min-h-screen bg-[black]">
+      <div className="flex flex-col gap-[min(2vw,20px)] px-[5%] lg:px-[26%] text-lightWhite font-latoRegular py-[max(20vw,100px)] lg:py-[max(15vh,100px)]">
+        {/* Title */}
+        <h1 className="font-avenirBlack text-center text-[8vw] lg:text-[3.3vw]">
+          {article.title}
+        </h1>
+        <h2 className="text-center text-[3vw] lg:text-[1vw]">
+          Author: {article.author} • Publish Date: {article.date}
+        </h2>
+        {/* Image */}
+        <div className="aspect-w-[994] aspect-h-[522] relative rounded-[20px] overflow-hidden">
+          <Image
+            src={article.image}
+            alt="article image"
+            width={2000}
+            height={2000}
+            className="absolute inset-0 w-full h-full object-cover"
+          />
+        </div>
+        {/* Article */}
+        <p className="mt-[20px] text-[3.3vw] lg:text-[1.2vw] text-justify">
+          {article.content}
+        </p>
+      </div>
+    </main>
+  );
+}
--- src/components/telescope/TelescopeBox.js
+++ src/components/telescope/TelescopeBox.js
@@ -1,57 +1,60 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export function TelescopeBox({ article, type, className }) {
   // type = "lg" or "sm"
   return (
-    <div
-      className={`w-full ${
-        type === "lg"
-          ? "aspect-w-[326] aspect-h-[154]"
-          : "aspect-w-[154] aspect-h-[106]"
-      } ${className}`}
-    >
+    <Link href={`/telescope/${article.id}`}>
       <div
-        className={`pl-[5%] ${
-          type === "lg" ? "pr-[27%]" : "pr-[12%]"
-        } py-[5%] rounded-[10px] lg:rounded-[20px]`}
+        className={`w-full ${
+          type === "lg"
+            ? "aspect-w-[326] aspect-h-[154]"
+            : "aspect-w-[154] aspect-h-[106]"
+        } ${className}`}
       >
-        {/* Background */}
-        <div className="z-10 absolute inset-0 rounded-[10px] overflow-hidden">
-          <div className="absolute inset-0 [background-image:linear-gradient(120deg,var(--tw-gradient-stops))] from-primary from-20% to-secondary to-80%" />
-          <div className="absolute inset-0 bg-black opacity-[0.35]" />
-          <Image
-            src={article.image}
-            alt="article image"
-            width={2000}
-            height={2000}
-            className="absolute inset-0 w-full h-full object-cover grayscale blur-[2px] opacity-20"
-          />
-        </div>
-        {/* Text */}
         <div
-          className={`z-20 relative h-full flex flex-col text-lightWhite ${
-            type === "lg" ? "justify-between lg:justify-end" : "justify-end"
-          }`}
+          className={`pl-[5%] ${
+            type === "lg" ? "pr-[27%]" : "pr-[12%]"
+          } py-[5%] rounded-[10px] lg:rounded-[20px]`}
         >
-          <div className="flex flex-col gap-[6px]">
-            <div
-              className={`font-avenirBlack ${
-                type === "lg" ? "text-[5vw]" : "text-[3.5vw]"
-              } leading-[1.2] line-clamp-2`}
-            >
-              {article.title}
-            </div>
-            {type === "lg" && (
-              <div className="font-latoRegular text-[2.7vw] leading-[1.2]">
-                {article.subtitle}
-              </div>
-            )}
+          {/* Background */}
+          <div className="z-10 absolute inset-0 rounded-[10px] overflow-hidden">
+            <div className="absolute inset-0 [background-image:linear-gradient(120deg,var(--tw-gradient-stops))] from-primary from-20% to-secondary to-80%" />
+            <div className="absolute inset-0 bg-black opacity-[0.35]" />
+            <Image
+              src={article.image}
+              alt="article image"
+              width={2000}
+              height={2000}
+              className="absolute inset-0 w-full h-full object-cover grayscale blur-[2px] opacity-20"
+            />
           </div>
-          <div className="lg:hidden italic underline text-[2.7vw] leading-[1.2]">
-            Read Article
+          {/* Text */}
+          <div
+            className={`z-20 relative h-full flex flex-col text-lightWhite ${
+              type === "lg" ? "justify-between lg:justify-end" : "justify-end"
+            }`}
+          >
+            <div className="flex flex-col gap-[6px]">
+              <div
+                className={`font-avenirBlack ${
+                  type === "lg" ? "text-[5vw]" : "text-[3.5vw]"
+                } leading-[1.2] line-clamp-2`}
+              >
+                {article.title}
+              </div>
+              {type === "lg" && (
+                <div className="font-latoRegular text-[2.7vw] leading-[1.2]">
+                  {article.subtitle}
+                </div>
+              )}
+            </div>
+            <div className="lg:hidden italic underline text-[2.7vw] leading-[1.2]">
+              Read Article
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
