7624cd2851a5f30685ea21c82a9e206f7625f48d
mie-intel <paryapradhanika@gmail.com>
2024-09-01 15:49:55 +0700

    feat[SecureYours]: menyelesaikan secure yours

--- src/app/(pages)/store/merch/page.js
+++ src/app/(pages)/store/merch/page.js
@@ -1,11 +1,13 @@
 import HeroMerch from "@/components/store/merch/HeroMerch";
+import SecureYours from "@/components/store/merch/SecureYours";
 import WhatTheySay from "@/components/store/merch/WhatTheySay";
 
 export default async function Casebook() {
   return (
-    <>
+    <main>
       <HeroMerch />
       <WhatTheySay />
-    </>
+      <SecureYours />
+    </main>
   );
 }
