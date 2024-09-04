894cb45068cd923e417412f49e6a3be1ed0d8426
hilmiwismadi <dzakiwismadi@gmail.com>
2024-09-04 12:40:33 +0700

    feat: mobile casebook journey

--- src/components/store/casebook/CasebookJourney.js
+++ src/components/store/casebook/CasebookJourney.js
@@ -66,68 +66,83 @@ export default function CasebookJourney() {
                 className="absolute -left-[5vw] top-[3vw] z-0 h-[35.313vw] w-[1.446vw] object-cover max-lg:hidden"
               />
               <div className="mb-[2vw] h-[15.625vw] w-[47.917vw] rounded-[1.25vw] bg-white px-[2vw] py-[2vw]">
-                <h1 className="font-avenirBlack text-[1.25vw] text-[#6FAA26] ">August 2021</h1>
-                <h1 className="font-avenirBlack text-[1.667vw] text-[#58B9D1]">Our Mini Casebook was published.</h1>
-                <h1 className="w-[42.917vw] font-latoRegular text-[1.25vw]/[1.25vw] text-black">
+                <h1 className="mt-[0.5vw] font-avenirBlack text-[1.35vw] text-[#6FAA26]">
+                  August 2021
+                </h1>
+                <h1 className="mt-[0.1vw] font-avenirBlack text-[1.867vw] text-[#58B9D1]">
+                  Our Mini Casebook was published.
+                </h1>
+                <h1 className="mt-[0.8vw] w-[42.917vw] font-latoRegular text-[1.25vw]/[1.35vw] text-black">
                   180 DC UGM Mini Casebook is our initial version of delivering Consulting 101
                   materials, as well as case interview practices and a step-by-step guide on how to
                   solve a typical business case, which was loved by thousands of you
                 </h1>
               </div>
-              <div className="mb-[2vw] h-[20.521vw] w-[47.917vw] rounded-[1.25vw] bg-white"> </div>
+
+              <div className="mb-[2vw] h-[20.521vw] w-[47.917vw] rounded-[1.25vw] bg-white px-[2vw] py-[2vw]">
+                <h1 className="mt-[0.5vw] font-avenirBlack text-[1.35vw] text-[#6FAA26]">
+                  January 2024 - Now
+                </h1>
+                <h1 className="mt-[0.1vw] font-avenirBlack text-[1.867vw] text-[#58B9D1]">
+                  Introducing Casebook Vol.2
+                </h1>
+                <h1 className="mt-[0.8vw] w-[42.917vw] font-latoRegular text-[1.25vw]/[1.35vw] text-black">
+                  Casebook Vol 2 aims to provide a walkthrough for readers on getting into
+                  Consulting companies, including CV, Cover Letter, and especially case-based
+                  interviews. In this newest version of 180DC UGM’s Casebook, SMEs in Yogyakarta
+                  become our subject for our thorough case practices, including market entry,
+                  profitability case practices, including market entry, profitability, supply chain
+                  efficiency, and employee retention case illustrations.
+                </h1>
+              </div>
             </div>
           </div>
         </div>
 
         {/* Mobile */}
-        <div className="z-[2] mb-[26.372vw] mt-[12.372vw] flex w-full flex-col items-center lg:hidden">
+        <div className="z-[2] mb-[6.372vw] mt-[6.372vw] flex w-full flex-col items-center lg:hidden">
           <div className="flex w-full flex-col items-center justify-center">
-            <h1 className="font-avenirHeavy text-[6.154vw] text-white"> What's Inside?</h1>
+            <div className="flex flex-col items-center justify-center mb-[7vw]">
+              <h1 className="font-avenirHeavy text-[6.154vw]/[6.3vw] text-white">CASEBOOK</h1>
+              <h1 className="font-avenirHeavy text-[6.154vw]/[6.3vw] text-white">Journey</h1>
+            </div>
 
-            <div className="mt-[10vw]">
-              <div className="relative flex h-[82.051vw] w-[81.538vw] flex-col items-center justify-center rounded-[4.103vw] bg-white">
-                <Image
-                  src="/img/store/casebook/gradientCardOrnament.png"
-                  alt="Gradient Card Ornament"
-                  width={2000}
-                  height={2000}
-                  className="absolute inset-0 left-[27.5vw] top-[12vw] h-[4.103vw] w-[25.641vw] object-cover lg:hidden"
-                />
-                <h1 className="mt-[6vw] w-[67.179vw] px-[2vw] text-center font-latoRegular text-[4.303vw]/[4.4vw]">
-                  <span>All you need to know to ace </span>
-                  <span className="font-bold underline">CV-making, Cover Letters,</span>
-                  <span>and </span>
-                  <span className="font-bold underline">Case-Based interview</span>
+            <div className="relative ml-[8vw]">
+              <Image
+                src="/img/store/casebook/LineBesideMobile.png"
+                alt="background"
+                width={2000}
+                height={2000}
+                className="absolute -left-[7vw] top-[6vw] z-0 h-[132.308vw] w-[4.103vw] object-cover lg:hidden"
+              />
+              <div className="mb-[4vw] h-[54.103vw] w-[79.487vw] rounded-[3.077vw] bg-white px-[5vw] py-[2vw]">
+                <h1 className="mt-[2.5vw] font-avenirBlack text-[3.377vw] text-[#6FAA26]">
+                  August 2021
+                </h1>
+                <h1 className="mt-[0.1vw] font-avenirBlack text-[3.79vw] text-[#58B9D1]">
+                  Our Mini Casebook was published.
+                </h1>
+                <h1 className="mt-[2.8vw] w-[69.231vw] font-latoBold text-[3.79vw]/[4.5vw] text-black">
+                  180 DC UGM Mini Casebook is our initial version of delivering Consulting 101
+                  materials, as well as case interview practices and a step-by-step guide on how to
+                  solve a typical business case, which was loved by thousands of you
                 </h1>
               </div>
-              <div className="relative mt-[5vw] flex h-[82.051vw] w-[81.538vw] flex-col items-center justify-center rounded-[4.103vw] bg-white">
-                <Image
-                  src="/img/store/casebook/gradientCardOrnament.png"
-                  alt="Gradient Card Ornament"
-                  width={2000}
-                  height={2000}
-                  className="absolute inset-0 left-[27.5vw] top-[12vw] h-[4.103vw] w-[25.641vw] object-cover lg:hidden"
-                />
-                <h1 className="mt-[6vw] w-[67.179vw] px-[2vw] text-center font-latoRegular text-[4.303vw]/[4.4vw]">
-                  <span>All you need to know to ace </span>
-                  <span className="font-bold underline">CV-making, Cover Letters,</span>
-                  <span>and </span>
-                  <span className="font-bold underline">Case-Based interview</span>
+
+              <div className="w-[79.487vw]] mb-[2vw] h-[80.256vw] rounded-[3.077vw] bg-white px-[5vw] py-[2vw]">
+                <h1 className="mt-[2.5vw] font-avenirBlack text-[3.377vw] text-[#6FAA26]">
+                  January 2024 - Now
                 </h1>
-              </div>
-              <div className="relative mt-[5vw] flex h-[82.051vw] w-[81.538vw] flex-col items-center justify-center rounded-[4.103vw] bg-white">
-                <Image
-                  src="/img/store/casebook/gradientCardOrnament.png"
-                  alt="Gradient Card Ornament"
-                  width={2000}
-                  height={2000}
-                  className="absolute inset-0 left-[27.5vw] top-[12vw] h-[4.103vw] w-[25.641vw] object-cover lg:hidden"
-                />
-                <h1 className="mt-[6vw] w-[67.179vw] px-[2vw] text-center font-latoRegular text-[4.303vw]/[4.4vw]">
-                  <span>All you need to know to ace </span>
-                  <span className="font-bold underline">CV-making, Cover Letters,</span>
-                  <span>and </span>
-                  <span className="font-bold underline">Case-Based interview</span>
+                <h1 className="mt-[0.1vw] font-avenirBlack text-[3.79vw] text-[#58B9D1]">
+                  Introducing Casebook Vol.2
+                </h1>
+                <h1 className="mt-[4    .8vw] w-[69.231vw] font-latoBold text-[3.79vw]/[4.5vw] text-black ">
+                  Casebook Vol 2 aims to provide a walkthrough for readers on getting into
+                  Consulting companies, including CV, Cover Letter, and especially case-based
+                  interviews. In this newest version of 180DC UGM’s Casebook, SMEs in Yogyakarta
+                  become our subject for our thorough case practices, including market entry,
+                  profitability case practices, including market entry, profitability, supply chain
+                  efficiency, and employee retention case illustrations.
                 </h1>
               </div>
             </div>
