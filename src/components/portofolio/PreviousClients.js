7681bca90b558135a7bcb7a90477664ef1310f66
hilmiwismadi <dzakiwismadi@gmail.com>
2024-10-26 17:22:16 +0700

    feat: all porto updated

--- src/components/portofolio/PreviousClients.js
+++ src/components/portofolio/PreviousClients.js
@@ -30,7 +30,6 @@ export default function PreviousClients() {
       {
         name: "Kopi Sembilan",
         nps: "100%",
-        backgr: "/img/portofolio/company_bgs/kopi_sembilan.png",
         logo: "/img/portofolio/company_logos/kopi_sembilan.png",
         desc: "Kopi Sembilan is a local coffee shop and one of the SMEs based on Yogyakarta.",
         pleft:
@@ -39,19 +38,124 @@ export default function PreviousClients() {
           "We helped them by restructurizing the hierarchy of Kopi Sembilan’s management. Our team also provided the integrated bookkeeping system by creating supply chain tracking management system",
         pright:
           "We provided organization structure, inventory management, and management controlling system which are more effective for financial reporting. It also affects for the better operational controlling and decision making in Kopi Sembilan.",
+        prightcondition: "impact",
       },
       {
         name: "Planet Protector Packaging",
         nps: "100%",
-        backgr: "/img/portofolio/company_bgs/planet_protector_packaging.png",
         logo: "/img/portofolio/company_logos/planet_protector_packaging.png",
         desc: "Planet Protector Packaging is the distributor of isolated sustainable, recyclable, biodegradable, and compost packaging - Woolpack to Australia and New Zealand.",
         pleft:
           "PPP needs advice to penetrate the Indonesian market as the part of their market expansion.",
         pcenter:
-          "We provided organization structure, inventory management, and management controlling syst",
+          "We developed eligible business model for PPP and go-to-market entry proposal to overcome the obstacles of the market entry which is supported by strategic partnership planning.",
         pright:
           "We had been able to secure several companies to make partnership with PPP and recommended strategy to approach the government. The insights that we had got, helped PPP to penetrate Indonesian market.",
+        prightcondition: "impact",
+      },
+      {
+        name: "Mercy Ships",
+        nps: "90%",
+        logo: "/img/portofolio/company_logos/mercy_ships.png",
+        desc: "Mercy Ships is an international charity that operates the largest non-governmental hospital ships in the world, providing humanitarian aid.",
+        pleft:
+          "Mercy Ships wanted to enter the Asian market, in particular: Indonesia, Vietnam, and Philippines market as a part of their broader expansion.",
+        pcenter:
+          "We developed a market and competitor analysis, as well as solution that focuses on website optimization strategy, consists of UI/UX and marketing strategy for each country.",
+        pright:
+          "We delivered strategy that Mercy Ships use to developed its website and social media. This include an implementation plan that consists of timeline, human resources needed, and metrics for KPI.",
+        prightcondition: "impact",
+      },
+      {
+        name: "Widya Edu",
+        nps: "100%",
+        logo: "/img/portofolio/company_logos/Widya_Edu.png",
+        desc: "Widya Edu is an education technology based company that aims to help Indonesian high school students pursue their college dream by practicing and consultation.",
+        pleft:
+          "Widya edu faced low explore and brand identification in the edu tech market. The project scope was surrounded in marketing.",
+        pcenter:
+          " We offered to provide Widya Edu in gaining exposure on the current red ocean edu tech market. This widened their market channel of  all students.",
+        pright:
+          " “Made it much easier to achieve our aims. We are Satisfied with the work! We are so impressed. Thank you! “.",
+        prightcondition: "comment",
+      },
+      {
+        name: "Asdos.id",
+        nps: "90%",
+        logo: "/img/portofolio/company_logos/Asdos.Id.jpg",
+        desc: "Asdos is an online marketplace platform aimed to provide academic assistance services for University students.",
+        pleft:
+          "Asdos.Id was faced with low brand recognition and needed to re-evaluated their current brand value and proposition.",
+        pcenter:
+          "We offered to provide consultation on the increasing of their current brand recognition of their current market segment, especially since they are about to re-launch..",
+        pright:
+          " “It was the best solution for social impact initiatives to receive help from many new insights from 180 Degrees Consulting team. It does not interfere with our day-to-day operations, it did not cost us a lot of resources, and it definitely helped us fill a gap in our company.”",
+        prightcondition: "comment",
+      },
+      {
+        name: "Kolom Remaja",
+        nps: "100%",
+        logo: "/img/portofolio/company_logos/KolomRemaja.png",
+        desc: "Kolom Remaja is an independent online media with a focus on promoting freedom of speech and expression especially for the Indonesian youth.",
+        pleft:
+          "Kolom Remaja was looking for ways to improve its human resources management and upscale its product development strategy.",
+        pcenter:
+          "We helped to analyze the brand perception of Kolom Remaja and created a development map to strengthen its brand personality. We also developed strategies to improve the synergies among its members.",
+        pright:
+          " “Great work on this and it was really insightful. Eventually someone was able to do market research and proper analysis to this!”",
+        prightcondition: "comment",
+      },
+      {
+        name: "Gores Denai",
+        nps: "100%",
+        logo: "/img/portofolio/company_logos/GoresDenai.png",
+        desc: "Gores Denai is an organization that aims to empower Indonesian youth to gain international exposure and maximize global opportunities especially in the education field.",
+        pleft:
+          "Gores Denai faced challenges in broadening its leverage in the market through its branding and gaining profits while simultaneously maintaining free product access to the audiences.",
+        pcenter:
+          "We conducted analysis on Gores Denai’s branding and recommended the sustainable strategic steps to attract customers and increase user engagement which includes the display of filler content and re-evaluation of its programs.",
+        pright:
+          " “180DC has been very helpful in navigating our organizational concerns. On top of that, the performance of the analyst were incredible as they constantly came up with highly practical and feasible solutions. 100% would recommend.”",
+        prightcondition: "comment",
+      },
+      {
+        name: "KitaBisa",
+        nps: "80%",
+        logo: "/img/portofolio/company_logos/KitaBisa.png",
+        desc: "KitaBisa.com is an online crowdfunding platform aim to start humanitarian initiatives, campaign and social programs.",
+        pleft:
+          "KitaBisa sought the ways to enhance their its branding eminence against its competitors.",
+        pcenter:
+          "We ran a diagnostic analysis on KitaBisa’s internal situation and offered solutions to strengthen the corporate value, such as strategies to enhance data collection, transparency for viewers, and fill the gaps in its social campaigns and program.",
+        pright:
+        " “The report they assisted us in developing not only met our expectations, but far exceeded them, reflecting their unwavering commitment to delivering top-tier work.” ",
+        prightcondition: "comment",
+      },
+      {
+        name: "EWasteRJ",
+        nps: "90%",
+        logo: "/img/portofolio/company_logos/EwasteRJ.jpg",
+        desc: "EWasteRJ is a waste management community that focuses on educating the society the risks of unsafe disposal of electronic waste and facilitating EWasteRJ dropbox as a contemporary container for electronic waste before running the disposal process.",
+        pleft:
+          "EWasteRJ was faced with challenges in earning revenues and gaining certainty due to the process of deciding its monetization strategy.",
+        pcenter:
+          "We offered a consultation service that help to develop meaningful partnerships, execute actions to increase brand awareness, identifying gaps and improvement opportunities to produce more tangible revenue stream.",
+        pright:
+        " We provided them 2 types of pitch deck to be used for corporate and educational parties, while also listing for potential partners that can be approached by EWasteRJ. We also developed positioning strategies that revolve around brand activation initiatives to differentiate EWasteRJ with another competitors.",
+        prightcondition: "impact",
+      },
+      {
+        name: "Tersalur",
+        nps: "100%",
+        logo: "/img/portofolio/company_logos/LogoTersalur.jpg",
+        desc: "Tersalur is a foundation that operates to develop the welfare and quality of human resources in Indonesia, especially those who are vulnerable because of being impacted by poverty, disaster, and diseases.",
+        pleft:
+          "Tersalur had difficulties in giving training and mentoring to its members, organize effective virtual teams, raise knowledge transfer awareness, and collect documentation.",
+        pcenter:
+          "We ran an analysis of Tersalur’s organizational structure, recruitment process, and competitors. More to that, we also recommended the strategies to conduct relevant upgrading program to improve both department and general technical skills.",
+        pright:
+        " “With an affordable price, I would highly recommend their services for other NGOs searching for ways to grow. 180DC not only focus on business profits but also impact which aligns with NGOs, giving their NGO clients well-aligned solutions.”",
+        prightcondition: "comment",
       },
     ],
     [],
@@ -151,11 +255,11 @@ export default function PreviousClients() {
                   <div className="absolute -z-[990] h-full w-full">
                     <div className="relative h-full w-full bg-[#2C6970]/[47%]">
                       <div className="absolute left-0 -z-[990] h-full w-[70vh]">
-                        <ImgF
+                        {/* <ImgF
                           alt={client.name + "_bg" + "_" + { idx }}
                           src={client.backgr}
                           heightPtg={"100%"}
-                        />
+                        /> */}
                       </div>
                     </div>
                   </div>
@@ -196,11 +300,11 @@ export default function PreviousClients() {
 
                       {/* Lower Body: Description */}
                       <div className="swiper-no-swiping flex h-[60%] w-full flex-col gap-[2vh] overflow-y-scroll">
-                        <p className="font-latoRegular text-[3.5vw] leading-[1.2] text-lightWhite text-justify">
+                        <p className="text-justify font-latoRegular text-[3.5vw] leading-[1.2] text-lightWhite">
                           <p className="mb-[0.6vw] font-avenirBlack text-primary">
                             What challenges does {client.name} face?
                           </p>
-                          {client.pright}
+                          {client.pleft}
                         </p>
                         <p className="font-latoRegular text-[3.5vw] leading-[1.2] text-lightWhite">
                           <p className="mb-[0.6vw] font-avenirBlack text-primary">
@@ -210,9 +314,11 @@ export default function PreviousClients() {
                         </p>
                         <p className="font-latoRegular text-[3.5vw] leading-[1.2] text-lightWhite">
                           <p className="mb-[0.6vw] font-avenirBlack text-primary">
-                            Impact that we deliver
+                            {client.prightcondition === "impact"
+                              ? "Impact that we deliver"
+                              : `${client.name} comments of our work`}
                           </p>
-                          {client.pleft}
+                          {client.pright}
                         </p>
                       </div>
 
@@ -260,7 +366,7 @@ export default function PreviousClients() {
               onSwiper={(swiper) => {
                 swiperRef.current = swiper;
               }}
-              spaceBetween={100}
+              spaceBetween={50}
               slidesPerView={2}
               autoplay={{
                 delay: 5000,
@@ -278,22 +384,25 @@ export default function PreviousClients() {
                       {/* Background */}
                       <div className="absolute h-[50%] w-full overflow-clip rounded-[1.25vw] rounded-bl-none 2xl:rounded-[19.2px]">
                         <div className="relative h-full w-full bg-[#0A1E22]/[47%]">
-                          <div className="absolute left-0 -z-[990] h-full w-full">
+                          {/* <div className="absolute left-0 -z-[990] h-full w-full">
                             <ImgF alt={client.name + "_bg" + "_" + { idx }} src={client.backgr} />
-                          </div>
+                          </div> */}
                         </div>
                       </div>
 
                       {/* Upper Body: Identity */}
                       <div className="relative mt-[1.1vw] flex h-[50%] w-full gap-[30px] p-[20px] 2xl:mt-[16.8px] 2xl:p-[23px]">
                         {/* Title & NPS */}
-                        <div className="flex h-full w-6/12 flex-col items-start justify-start relative">
+                        <div className="relative flex h-full w-6/12 flex-col items-start justify-start">
                           <p className="font-avenirBlack text-[2vw] leading-none text-primary 2xl:text-[30.7px]">
                             {client.name}
                           </p>
-                          <p className="font-avenirLight text-[1.2vw] leading-none text-secondary 2xl:text-[18.4px] mt-[1vw]">
+                          <p className="mt-[1vw] font-avenirLight text-[1.2vw] leading-none text-secondary 2xl:text-[18.4px]">
                             <h2 className="font-avenirBlack text-lightWhite"> {client.desc} </h2>
-                            <strong className="absolute font-avenirHeavy text-[1.9vw] bottom-[0] "> NPS of {client.nps}</strong>
+                            <strong className="absolute bottom-[0] font-avenirHeavy text-[1.9vw]">
+                              {" "}
+                              NPS of {client.nps}
+                            </strong>
                           </p>
                         </div>
 
@@ -306,22 +415,25 @@ export default function PreviousClients() {
                       </div>
                       {/* Lower Body: Description */}
                       <div className="mt-[1.2vw] flex h-full w-full gap-[2.4vw] px-[20px] pb-[20vw] 2xl:mt-[18px] 2xl:px-[23px]">
-                        <p className="w-4/12 font-latoRegular text-[1vw] leading-[1.2] text-lightWhite 2xl:text-[15.36px] text-justify">
+                        <p className="w-4/12 text-justify font-latoRegular text-[1vw] leading-[1.2] text-lightWhite 2xl:text-[15.36px]">
                           <p className="mb-[0.6vw] font-avenirBlack text-primary">
                             What challenges does {client.name} face?
                           </p>
                           {client.pleft}
                         </p>
-                        <p className="w-4/12 font-latoRegular text-[1vw] leading-[1.2] text-lightWhite 2xl:text-[15.36px] text-justify">
+                        <p className="w-4/12 text-justify font-latoRegular text-[1vw] leading-[1.2] text-lightWhite 2xl:text-[15.36px]">
                           <p className="mb-[0.6vw] font-avenirBlack text-primary">
                             What solution does we offer?
                           </p>
                           {client.pcenter}
                         </p>
-                        <p className="w-4/12 font-latoRegular text-[1vw] leading-[1.2] text-lightWhite 2xl:text-[15.36px] text-justify">
+                        <p className="w-4/12 text-justify font-latoRegular text-[1vw] leading-[1.2] text-lightWhite 2xl:text-[15.36px]">
                           <p className="mb-[0.6vw] font-avenirBlack text-primary">
-                            Impact that we deliver
+                            {client.prightcondition === "impact"
+                              ? "Impact that we deliver"
+                              : `${client.name} comments of our work`}
                           </p>
+
                           {client.pright}
                         </p>
                       </div>
