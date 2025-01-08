f64fe553d14d809084c42f67844857e47a093fd0
hilmiwismadi <dzakiwismadi@gmail.com>
2025-01-08 08:38:43 +0700

    feat: timeline update

--- src/app/(pages)/cycle2oprec/page.js
+++ src/app/(pages)/cycle2oprec/page.js
@@ -1,11 +1,13 @@
 "use client";
 
 import { HeroOprec } from "@/components/cycle2oprec/HeroOprec";
+import { TimelineOprec } from "@/components/cycle2oprec/TimelineOprec";
 
 export default function Cycle2Oprec() {
   return (
     <main className="bg-[#E8E8E8]">
       <HeroOprec/>
+      <TimelineOprec />
     </main>
   );
 }
--- src/components/cycle2oprec/HeroOprec.js
+++ src/components/cycle2oprec/HeroOprec.js
@@ -58,8 +58,8 @@ export function HeroOprec() {
             {/* bagian enroll */}
             <div className="mt-[3.646vw] hidden w-full flex-col items-center px-4 lg:mt-[1.8vw] lg:flex">
               <div>
-                <p className="text-[4vw] text-white180 lg:text-[1.25vw]">
-                  Lorem Ipsum <br /> Lorem Ipsum Lorem Ipsum Lorem Ipsum
+                <p className="text-[4vw] font-latoSemibold text-white180 lg:text-[1.25vw] w-[30vw]">
+                Calling all aspiring consultants and changemakers! <br /> Join 180DC UGM and leave your mark.
                 </p>
                 <div className="flex gap-2">
                   <Button
@@ -81,14 +81,34 @@ export function HeroOprec() {
           </div>
 
           {/* bagian kanan */}
-          <div className="flex flex-col lg:w-1/2">
+          <div className="flex flex-col items-center justify-center max-lg:my-[14vw] max-lg:scale-[1.5] lg:w-1/2">
             <Image
               src="/img/opreccycle/RightLetterImage.png"
               alt="background"
               width={2000}
               height={2000}
-              className="inset-0 z-10 aspect-[681/715] w-[30vw] "
+              className="aspect-[681/715] z-10 ml-[2vw] w-[30vw] max-lg:w-[40vw]"
             />
+          </div>
+
+          <div className="mt-[5.938vw] flex w-full flex-col justify-center items-center px-[8.667vw] lg:hidden gap-y-[2vw]">
+          <p className="font-latoSemibold text-white180 text-[3vw] w-[70vw] flex  text-center justify-center">
+                Calling all aspiring consultants and changemakers! <br /> Join 180DC UGM and leave your mark.
+                </p>
+            <div className="flex w-full items-center justify-center gap-[2vw]">
+              <Button
+                color="white"
+                text="Registration"
+                addClass="w-[40%]  h-fit py-[1.563vw] text-[2.9vw] mt-[2.154vw] lg:mt-[1.25vw] font-bold"
+                href="bit.ly/RegistrationCB180DC"
+              />
+              <Button
+                color="green"
+                text="Guidebook"
+                addClass="w-[40%]  h-fit py-[1.563vw] text-[2.9vw] mt-[2.154vw] lg:mt-[1.25vw] font-bold"
+                href="bit.ly/GuidebookCB180DC"
+              />
+            </div>
           </div>
         </div>
       </div>
