07f6d4b75823771e8c367c0514083a9a5016656c
mie-intel <paryapradhanika@gmail.com>
2025-01-13 21:29:09 +0700

    fix[Term Change]: membenarkan term change

--- src/components/aboutus/Team.js
+++ src/components/aboutus/Team.js
@@ -14,16 +14,14 @@ import { IoTriangle } from "react-icons/io5";
 export default function Team() {
   // Content
   const terms = ["2024/2025", "2023/2024"];
-  const [data, setData] = useState({ term: terms[0], open: false });
+  const [term, setTerm] = useState(terms[0]);
+  const [open, setOpen] = useState(false);
   const [hydrate, setHydrate] = useState(false);
-  let currBoD = null;
 
   const handleChangeTerm = async (item) => {
     if (hydrate) {
-      setData((prevData) => ({
-        term: item,
-        open: prevData.term === item ? !prevData.open : true, // Correctly toggling `open` based on the previous state
-      }));
+      setTerm(item);
+      setOpen(!open);
     }
   };
 
@@ -48,25 +46,20 @@ export default function Team() {
                 </h1>
                 <button
                   type="button"
-                  value={data.term}
+                  value={term}
                   onClick={() => {
-                    setData((prevData) => {
-                      return {
-                        ...prevData,
-                        open: !prevData.open,
-                      };
-                    });
+                    setOpen(!open);
                   }}
                   className="relative z-[1] mt-[4%] flex aspect-[383/74] w-[40%] flex-row items-center justify-center rounded-[5%/20%] border-[2px] border-[#58B9D1] px-[2%] py-[0.5%] text-center font-avenirBook text-[2.6vw] font-[700] text-secondary outline-0 md:w-[36%] md:border-[3px] lg:mt-[2%] lg:w-[16%] lg:text-[1.4vw] 2xl:text-[20px]"
                 >
                   <p className="flex w-full flex-row items-center justify-evenly gap-[5%] text-right">
-                    {data.term}
+                    {term}
                   </p>
                   <IoTriangle
-                    className={`w-[8%] duration-500 ${data.open ? "rotate-0" : "rotate-180"}`}
+                    className={`w-[8%] duration-500 ${open ? "rotate-0" : "rotate-180"}`}
                   />
                   <div
-                    className={`${data.open ? "flex -translate-y-0 flex-col opacity-100" : "z-[-1] -translate-y-[100%] bg-[red] opacity-0"} absolute left-0 top-[101%] z-[12] h-fit w-full gap-0 rounded-b-lg border-[2px] border-[#58B9D1] py-[1%] transition-all duration-500 md:border-[3px]`}
+                    className={`${open ? "flex -translate-y-0 flex-col opacity-100" : "z-[-1] -translate-y-[100%] bg-[red] opacity-0"} absolute left-0 top-[101%] z-[12] h-fit w-full gap-0 rounded-b-lg border-[2px] border-[#58B9D1] py-[1%] transition-all duration-500 md:border-[3px]`}
                   >
                     {terms.map((item) => (
                       <button
@@ -97,7 +90,7 @@ export default function Team() {
             {/* Cards Row */}
             <div data-gsap="up" className="relative mt-[3.6%] flex w-[60%] flex-col gap-[2%]">
               {hydrate &&
-                BoD[data.term].map((row) => {
+                BoD[term].map((row) => {
                   return (
                     <>
                       <div
