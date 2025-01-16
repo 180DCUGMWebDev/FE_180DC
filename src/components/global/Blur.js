de9409b430965eedbe210e462260195c6e89d517
mie-intel <paryapradhanika@gmail.com>
2025-01-16 21:11:07 +0700

    fix[Blur]: memperbaiki blur logic

--- src/components/global/Blur.js
+++ src/components/global/Blur.js
@@ -1,39 +1,66 @@
 "use client";
 
 import { AuthContext } from "@/contexts/AuthContext";
-import { useContext, useRef } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
+import { BiSolidSend } from "react-icons/bi";
+import "./Blur.css";
 
 export default function Blur({ className, children, isBlur }) {
-  const { isLogin } = useContext(AuthContext);
+  const { isLogin, setIsLogin } = useContext(AuthContext);
   const inputRef = useRef();
   let currentEmail = "";
   const submitEmail = (value) => {
     currentEmail = value;
-    console.log(value);
+    setIsLogin(true);
+  };
+  const [open, setOpen] = useState(true);
+  const [hydrate, setHydrate] = useState(false);
+  useEffect(() => {
+    setHydrate(true);
+    setIsLogin(true);
+  }, []);
+  const handleOpen = () => {
+    hydrate && !open && setOpen(true);
   };
   return (
     <div className={`relative overflow-hidden ${className}`}>
-      {/* {(isBlur || isLogin) && (
+      {isBlur && !isLogin && (
         <>
-          <div className="absolute left-[-7.5%] top-[-7.5%] z-[100] h-[115%] w-[115%] bg-[url('/img/academy/GRAD_180Mobile.png')] bg-cover opacity-[0.99] blur-xl md:left-[-2.5%] md:top-[-2.5%] md:h-[105%] md:w-[105%] md:opacity-[0.99] md:blur-xl" />
-          <div className="absolute bottom-0 left-0 right-0 top-0 z-[101] m-auto flex h-[80%] w-[85%] flex-col items-center justify-center rounded-[2%/3.5%] bg-[#D9D9D9] blur-none">
-            <h3 className="mb-[2%] font-avenirBlack text-[3.2vw] text-[#7BBA74] md:text-[3.1vw] lg:text-[1.5vw]">
-              Insert your email, connect with us
-            </h3>
-            <input
-              ref={inputRef}
-              onKeyDown={(e) => {
-                if (e.key == "Enter") {
-                  submitEmail(e.target.value);
-                  inputRef.current.value = null;
-                }
-              }}
-              className="h-[14%] w-[75%] rounded-[5%/48%] bg-[#C3C2C0] px-[3%] outline-0"
-            />
-            {currentEmail}
-          </div>
+          <div
+            onMouseEnter={handleOpen}
+            onClick={handleOpen}
+            className="absolute left-[-7.5%] top-[-7.5%] z-[100] h-[115%] w-[115%] bg-[#58B9D1] bg-cover opacity-[0.99] blur-md md:left-[-2.5%] md:top-[-2.5%] md:h-[105%] md:w-[105%] md:opacity-[0.99] md:blur-xl"
+          />
+          {hydrate && (
+            <div
+              className={`${open ? "openZ translate-y-0 opacity-100" : "closeZ translate-y-[50%] opacity-0"} absolute bottom-0 left-0 right-0 top-0 z-[101] m-auto flex h-[40%] w-[75%] flex-col items-center justify-center rounded-[2.6%/8%] bg-white blur-none transition-all duration-1000`}
+            >
+              <h3 className="mb-[2%] font-avenirBook text-[3.2vw] font-[700] text-[#7BBA74] md:text-[3.2vw] lg:text-[1.6vw]">
+                Let's stay connected
+              </h3>
+              <p className="flex h-[27%] w-[85%] items-center justify-evenly rounded-[2%/17%] bg-[#EEEEEE] px-[3%] text-[2.2vw] md:text-[2.4vw] lg:text-[1vw]">
+                <input
+                  ref={inputRef}
+                  onKeyDown={(e) => {
+                    if (e.key == "Enter") {
+                      submitEmail(e.target.value);
+                      inputRef.current.value = null;
+                    }
+                  }}
+                  placeholder="Type your email here..."
+                  className="h-full w-full bg-transparent pr-[4%] outline-none"
+                />
+                <div className="mb-[1%] aspect-square w-[5%] rotate-[-45deg]">
+                  <BiSolidSend
+                    style={{ color: "#58B9D1" }}
+                    className="aspect-[1/1] h-full w-full"
+                  />
+                </div>
+              </p>
+            </div>
+          )}
         </>
-      )} */}
+      )}
 
       {children}
     </div>
