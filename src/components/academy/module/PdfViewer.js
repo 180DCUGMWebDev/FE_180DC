37ca2ba6254e1ee1429f42a7f244e2dc3f4e5736
mie-intel <paryapradhanika@gmail.com>
2025-02-19 18:13:23 +0700

    fix(PdfViewer)

--- src/components/academy/module/PdfViewer.js
+++ src/components/academy/module/PdfViewer.js
@@ -1,21 +1,22 @@
 "use client";
-import { Viewer, Worker } from "@react-pdf-viewer/core";
-import "@react-pdf-viewer/core/lib/styles/index.css";
-import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
-import "@react-pdf-viewer/default-layout/lib/styles/index.css";
+// import { Viewer, Worker } from "@react-pdf-viewer/core";
+// import "@react-pdf-viewer/core/lib/styles/index.css";
+// import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
+// import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 const PdfViewer = ({ url }) => {
   const defaultLayoutPluginInstance = defaultLayoutPlugin();
   return (
     <section className="relative">
-        <div className="pt-[9.92vw] lg:mt-[9.92vw] px-[9%] lg:px-[9%] mx-auto bg-black overflow-y-auto max-h-[90vh]">
-        <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.10.111/build/pdf.worker.min.js">
+      <div className="mx-auto max-h-[90vh] overflow-y-auto bg-black px-[9%] pt-[9.92vw] lg:mt-[9.92vw] lg:px-[9%]">
+        {/* <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.10.111/build/pdf.worker.min.js">
             <Viewer
             fileUrl={url}
             plugins={[defaultLayoutPluginInstance]}
             renderMode="canvas"
             />
-        </Worker>
-        </div>
+        </Worker> */}
+        <iframe src={url} className="h-full w-full" />
+      </div>
     </section>
   );
 };
