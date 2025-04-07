ac3bef8c62a4f99225df28f67d26f745b3303b98
hilmiwismadi <dzakiwismadi@gmail.com>
2025-04-07 15:11:25 +0700

    feat: secure yours fbank

--- src/app/(pages)/store/frameworkbank/page.js
+++ src/app/(pages)/store/frameworkbank/page.js
@@ -1,12 +1,13 @@
 import HeroFbanks from "@/components/store/frameworkbank/HeroFBanks";
 import PreviewFbanks from "@/components/store/frameworkbank/PreviewFbanks";
+import SecureYoursFbank from "@/components/store/frameworkbank/SecureYoursFbank";
 
 export default function frameworkbank() {
   return (
     <main>
         <HeroFbanks />
         <PreviewFbanks />
- 
+        <SecureYoursFbank />
     </main>
   );
 }
