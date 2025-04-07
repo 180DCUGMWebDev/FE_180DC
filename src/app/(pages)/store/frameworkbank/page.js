111efb51f82fd1204d303bd51b4be4cc38adeb23
hilmiwismadi <dzakiwismadi@gmail.com>
2025-04-07 15:09:28 +0700

    feat: preview Fbanks

--- src/app/(pages)/store/frameworkbank/page.js
+++ src/app/(pages)/store/frameworkbank/page.js
@@ -1,9 +1,11 @@
 import HeroFbanks from "@/components/store/frameworkbank/HeroFBanks";
+import PreviewFbanks from "@/components/store/frameworkbank/PreviewFbanks";
 
 export default function frameworkbank() {
   return (
     <main>
         <HeroFbanks />
+        <PreviewFbanks />
  
     </main>
   );
