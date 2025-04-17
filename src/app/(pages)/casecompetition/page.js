d799efe46636dd680f70c5aa863af8526c78a86a
hilmiwismadi <dzakiwismadi@gmail.com>
2025-04-17 15:09:48 +0700

    feat: hiding link

--- src/app/(pages)/casecompetition/page.js
+++ src/app/(pages)/casecompetition/page.js
@@ -6,8 +6,8 @@ import ComingSoon from "@/components/casecomp/ComingSoon";
 export default function CaseComp() {
   return (
     <main className="bg-[#E8E8E8]">
-      <FormCaseComp />
-      {/* <ComingSoon /> */}
+      {/* <FormCaseComp /> */}
+      <ComingSoon />
     </main>
   );
 }
