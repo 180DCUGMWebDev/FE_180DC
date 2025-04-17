0b55dd5205e353da896688e64f5a156cf5b4b712
hilmiwismadi <dzakiwismadi@gmail.com>
2025-04-17 15:01:08 +0700

    feat: timeline apac setup

--- src/app/(pages)/casecompetition/landingcc/page.js
+++ src/app/(pages)/casecompetition/landingcc/page.js
@@ -3,11 +3,13 @@
 import { FormCaseComp } from "@/components/casecomp/FormCaseCompetition";
 import ComingSoon from "@/components/casecomp/ComingSoon";
 import { HeroCaseComps } from "@/components/casecomp/HeroCaseComp";
+import {TimelineCaseComp} from "@/components/casecomp/TimelineCaseComp";
 
 export default function CaseComp() {
   return (
     <main className="bg-[#E8E8E8]">
-        <HeroCaseComps />
+      <HeroCaseComps />
+      <TimelineCaseComp />
       <ComingSoon />
     </main>
   );
