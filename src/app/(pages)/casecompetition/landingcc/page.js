3211308a97948c45a6c43d0746e1a7dbbc22b7c9
hilmiwismadi <dzakiwismadi@gmail.com>
2025-04-18 13:28:05 +0700

    feat: apa desc setup

--- src/app/(pages)/casecompetition/landingcc/page.js
+++ src/app/(pages)/casecompetition/landingcc/page.js
@@ -4,11 +4,13 @@ import { FormCaseComp } from "@/components/casecomp/FormCaseCompetition";
 import ComingSoon from "@/components/casecomp/ComingSoon";
 import { HeroCaseComps } from "@/components/casecomp/HeroCaseComp";
 import {TimelineCaseComp} from "@/components/casecomp/TimelineCaseComp";
+import { DescriptionCaseComp } from "@/components/casecomp/DescriptionCaseComp";
 
 export default function CaseComp() {
   return (
     <main className="bg-[#E8E8E8]">
       <HeroCaseComps />
+      <DescriptionCaseComp />
       <TimelineCaseComp />
     </main>
   );
--- src/components/casecomp/TimelineCaseComp.js
+++ src/components/casecomp/TimelineCaseComp.js
@@ -43,23 +43,22 @@ export function TimelineCaseComp() {
             className="z-5 absolute -right-[18vw] -top-[10vw] w-[24.008vw] object-cover max-lg:hidden"
           />
           {/* Centered Timeline image */}
-          <div className="font-latoBold text-[3.33vw] text-[#73B743] mt-[3vw] mb-[2vw]">
+          <div className="mb-[2vw] mt-[3vw] font-latoBold text-[3.33vw] text-[#73B743]">
             <Image
-            src="/img/casecomp/apactimelinetitle.png"
-            alt=""
-            width={2000}
-            height={2000}
-            className="w-[24.008vw] object-cover "
-          />
+              src="/img/casecomp/apactimelinetitle.png"
+              alt=""
+              width={2000}
+              height={2000}
+              className="w-[24.008vw] object-cover"
+            />
           </div>
-
 
           <Image
             src="/img/casecomp/TimelineAPAC.png" // Replace with your image path
             alt="Centered Image"
             width={2000}
             height={2000}
-            className="w-[80vw] max-w-full object-cover lg:w-[40vw] mb-[10vw]"
+            className="mb-[10vw] w-[80vw] max-w-full object-cover lg:w-[40vw]"
           />
         </div>
       </div>
