63246d4f38cdf1583a55fb4432c3c5dd195b9436
Nasta <khoirunasmuhammad@gmail.com>
2025-08-12 23:52:34 +0700

    feat(closed): closed reg

--- src/app/(pages)/oprec/25-26/functional/page.js
+++ src/app/(pages)/oprec/25-26/functional/page.js
@@ -1,15 +1,17 @@
-import HeroOprec from "@/components/modules/oprec/25-26/functional/HeroOprec";
-import Guidebook from "@/components/modules/oprec/25-26/functional/Guidebook";
-import CTA from "@/components/modules/oprec/25-26/functional/CTA";
+// import HeroOprec from "@/components/modules/oprec/25-26/functional/HeroOprec";
+// import Guidebook from "@/components/modules/oprec/25-26/functional/Guidebook";
+// import CTA from "@/components/modules/oprec/25-26/functional/CTA";
 // import ComingSoon from "@/components/modules/comingsoon/ComingSoon";
+import Closed from "@/components/modules/closed/Closed";
 
 export default function OpenRecruitment2526() {
   return (
     <>
       {/* <ComingSoon /> */}
-      <HeroOprec />
+      <Closed />
+      {/* <HeroOprec />
       <Guidebook />
-      <CTA />
+      <CTA /> */}
     </>
   );
 }
