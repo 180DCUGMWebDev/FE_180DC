219d70e7afa6836e60645184a28cc7b89c7b7fd9
Nasta <khoirunasmuhammad@gmail.com>
2025-08-18 23:50:40 +0700

    fix

--- src/app/(pages)/oprec/25-26/consulting/page.js
+++ src/app/(pages)/oprec/25-26/consulting/page.js
@@ -2,13 +2,15 @@ import HeroOprec from "@/components/modules/oprec/25-26/consulting/HeroOprec";
 import Guidebook from "@/components/modules/oprec/25-26/consulting/Guidebook";
 import CTA from "@/components/modules/oprec/25-26/consulting/CTA";
 // import ComingSoon from "@/components/modules/comingsoon/ComingSoon";
+import Closed from "@/components/modules/closed/Closed";
 
 export default function OpenRecruitment2526() {
   return (
     <>
-      <HeroOprec />
+      {/* <HeroOprec />
       <Guidebook />
-      <CTA />
+      <CTA /> */}
+      <Closed />
       {/* <ComingSoon /> */}
     </>
   );
