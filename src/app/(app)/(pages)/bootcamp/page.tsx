5cb96bb12d71e80342aafb3860844e0ca13fce31
Nasta <khoirunasmuhammad@gmail.com>
2025-11-09 21:22:09 +0700

    fix(button): fix button

--- src/app/(app)/(pages)/bootcamp/page.tsx
+++ src/app/(app)/(pages)/bootcamp/page.tsx
@@ -1,7 +1,6 @@
 // app/bootcamp/page.tsx (Server Component)
 import { Hero, Timeline, GuideBookBootcamp, Speaker } from "@/components/modules/bootcamp";
 import SecureYours from "@/components/modules/store/casebook/SecureYours";
-import Container from "@/components/layout/Container";
 import AOSInit from "@/components/AOSInit"; // komponen baru
 
 export const metadata = {
@@ -10,15 +9,15 @@ export const metadata = {
 
 export default function Bootcamp() {
   return (
-    <main className="bg-[#C4E2E4]">
-      <AOSInit />
-      <Hero />
-      <Speaker />
-      <Timeline />
-      <GuideBookBootcamp />
-      <Container className="hidden min-w-screen h-auto px-0 sm:px-0 py-0 lg:block">
-        <SecureYours theme="dark"/>
-      </Container>
-    </main>
+    <>
+      <main className="bg-[#C4E2E4]">
+        <AOSInit />
+        <Hero />
+        <Speaker />
+        <Timeline />
+        <GuideBookBootcamp />
+        <SecureYours theme="dark" />
+      </main>
+    </>
   );
 }
