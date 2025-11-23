1b443ae0ae32ccd3ceb8de4b656683ab37973d41
Nasta <khoirunasmuhammad@gmail.com>
2025-11-23 08:48:57 +0700

    feat(bootcamp): close registration of bootcamp

--- src/app/(app)/(pages)/bootcamp/registration/page.tsx
+++ src/app/(app)/(pages)/bootcamp/registration/page.tsx
@@ -1,4 +1,5 @@
 import RegistrationForm from "@/components/modules/bootcamp/registration/RegistrationForm";
+import Closed from "@/components/modules/closed/Closed";
 
 export const metadata = {
   title: "Bootcamp | 180 Degrees Consulting UGM",
@@ -6,12 +7,14 @@ export const metadata = {
 
 export default function BootcampRegistrationPage() {
   return (
-    <section className="flex min-h-screen items-center justify-center bg-gradient-to-br from-black to-green-300/90 p-4">
-      <div className="w-full max-w-4xl py-20">
-        <div className="rounded-lg border-0 bg-white p-6 shadow-2xl backdrop-blur-xs">
-          <RegistrationForm />
-        </div>
-      </div>
-    </section>
+    // <section className="flex min-h-screen items-center justify-center bg-gradient-to-br from-black to-green-300/90 p-4">
+    //   <div className="w-full max-w-4xl py-20">
+    //     <div className="rounded-lg border-0 bg-white p-6 shadow-2xl backdrop-blur-xs">
+    //       <RegistrationForm />
+
+    //     </div>
+    //   </div>
+    // </section>
+    <Closed />
   );
 }
