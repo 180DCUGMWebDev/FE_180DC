f609aae094fae745d73510b363b1353462c559f4
Nasta <khoirunasmuhammad@gmail.com>
2026-01-26 15:31:11 +0700

    feat: close rec

--- src/app/(app)/(pages)/oprec/25-26/consulting/cycle2/page.tsx
+++ src/app/(app)/(pages)/oprec/25-26/consulting/cycle2/page.tsx
@@ -1,16 +1,16 @@
 import HeroOprec from "@/components/modules/oprec/25-26/consulting/HeroOprec";
 import Guidebook from "@/components/modules/oprec/25-26/consulting/Guidebook";
-import CTA from "@/components/modules/oprec/25-26/consulting/CTA";
+// import CTA from "@/components/modules/oprec/25-26/consulting/CTA";
 // import ComingSoon from "@/components/modules/comingsoon/ComingSoon";
-// import Closed from "@/components/modules/closed/Closed";
+import Closed from "@/components/modules/closed/Closed";
 
 export default function OpenRecruitment2526() {
   return (
     <>
-      <HeroOprec />
-      <Guidebook />
-      <CTA registerPath="/oprec/25-26/consulting/cycle2/register" />
-      {/* <Closed /> */}
+      {/* <HeroOprec />
+      <Guidebook /> */}
+      {/* <CTA registerPath="/oprec/25-26/consulting/cycle2/register" /> */}
+      <Closed />
       {/* <ComingSoon /> */}
     </>
   );
