64f85932fa22b32c69bb3fa5e4df32718036be31
ancung <ancungaulia@gmail.com>
2026-04-18 17:38:40 +0700

    chore: delete unnecesarry files

--- src/data/casebook.ts
+++
@@ -1,8 +0,0 @@
-export const casebook = [
-    {
-        name: "Casebook [Softcopy]",
-        type: "digital" as const,
-        price: 59000,
-        imagePath: "",
-    },
-]
--- src/data/framework.ts
+++
@@ -1,8 +0,0 @@
-export const framework = [
-    {
-        name: "Framework Bank [Softcopy]",
-        type: "digital" as const,
-        price: 99000,
-        imagePath: "",
-    },
-]
